5857e675c1332fdf8fdae64adfd4b18f877f6085
Rohit708973 <rohit121889@gmail.com>
2026-01-01 09:20:21 +0530

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,4 @@
-import requests
+import cloudscraper
 from bs4 import BeautifulSoup
 import config
 
@@ -14,7 +14,8 @@ import base64
 
 class ECAPScraper:
     def __init__(self):
-        self.session = requests.Session()
+        self.session = cloudscraper.create_scraper(browser='chrome')
+
         self.headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
         }
